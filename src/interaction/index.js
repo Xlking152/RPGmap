feb407a486782cb78d71ab947df5a950f759b89c
王鑫磊 <64979279+Xlking152@users.noreply.github.com>
2026-08-22 13:13:20 +0800

    feat: export generic feature interaction api

---
+++ src/interaction/index.js
@@ -0,0 +1,11 @@
+export {
+  FEATURE_ACTION_META,
+  FEATURE_INTERACTION_STATE_KEY,
+  damageFeatureState,
+  featureInteractionSnapshot,
+  getFeatureInteractionState,
+  listFeatureInteractions,
+  setFeatureOpenState,
+} from './model.js';
+
+export { createFeatureInteractionSystem } from './system.js';
