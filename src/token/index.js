6c335a29996f2c225973d103afebeb75ef6abf77
王鑫磊 <64979279+Xlking152@users.noreply.github.com>
2026-08-27 16:19:45 +0800

    refactor(token): export Synthetic Actor helpers

--- src/token/index.js
+++ src/token/index.js
@@ -7,4 +7,5 @@ export {
   removeSceneToken,
   updateSceneToken,
 } from './model.js';
+export { mergeActorDelta, mergeActorDeltaPatch, resolveTokenActor } from './actor.js';
 export { createTokenRuntimeSystem } from './system.js';
