b0e5e80e4159ca82ee70552b04cd42cd2b7484a4
王鑫磊 <64979279+Xlking152@users.noreply.github.com>
2026-08-27 15:11:37 +0800

    feat(ruleset): move infinite horror actor definitions

---
+++ src/rulesets/infinite-horror/definitions.js
@@ -0,0 +1,29 @@
+export const INFINITE_HORROR_RESOURCE_DEFS = Object.freeze([
+  { id: 'hp', name: '生命', kind: 'hp' },
+  { id: 'stamina', name: '精力', kind: 'stamina' },
+  { id: 'willpower', name: '意志', kind: 'willpower' },
+]);
+
+export const INFINITE_HORROR_BAD_STATUS_DEFS = Object.freeze([
+  { id: 'bad-status-32', name: '冻结点数', group: 0 },
+  { id: 'bad-status-33', name: '失速点数', group: 0 },
+  { id: 'bad-status-34', name: '燃烧点数', group: 0 },
+  { id: 'bad-status-35', name: '纠缠点数', group: 0 },
+  { id: 'bad-status-36', name: '恶心点数', group: 1 },
+  { id: 'bad-status-37', name: '晶化点数', group: 1 },
+  { id: 'bad-status-38', name: '麻痹点数', group: 1 },
+  { id: 'bad-status-39', name: '剧痛点数', group: 1 },
+  { id: 'bad-status-40', name: '眩晕点数', group: 1 },
+  { id: 'bad-status-41', name: '肢体妨碍', group: 2 },
+  { id: 'bad-status-42', name: '流血点数', group: 2 },
+  { id: 'bad-status-43', name: '疲乏点数', group: 2 },
+  { id: 'bad-status-44', name: '耳鸣点数', group: 3 },
+  { id: 'bad-status-45', name: '目眩点数', group: 3 },
+  { id: 'bad-status-46', name: '沮丧点数', group: 4 },
+  { id: 'bad-status-47', name: '亢奋点数', group: 4 },
+  { id: 'bad-status-48', name: '恐惧点数', group: 4 },
+  { id: 'bad-status-49', name: '仇恨点数', group: 4 },
+  { id: 'bad-status-50', name: '欲眠点数', group: 4 },
+  { id: 'bad-status-51', name: '精神束缚', group: 4 },
+  { id: 'bad-status-52', name: '魅惑点数', group: 5 },
+]);
