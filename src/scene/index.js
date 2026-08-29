265eb9f56bdcb30effdea29fea8cfa6b5f433b39
王鑫磊 <64979279+Xlking152@users.noreply.github.com>
2026-08-30 00:25:49 +0800

    feat: export scene area handle system

--- src/scene/index.js
+++ src/scene/index.js
@@ -1,2 +1,4 @@
+export { applyAreaHandleDrag, areaHandlePoints } from './area-handle-geometry.js';
+export { createSceneAreaHandleSystem } from './area-handles.js';
 export { createSceneAreaSystem } from './areas.js';
 export { createSceneManagerSystem } from './manager.js';
