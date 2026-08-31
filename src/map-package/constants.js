b2742598095b6a097ebb366bdb401759a0c0a151
王鑫磊 <64979279+Xlking152@users.noreply.github.com>
2026-08-31 20:45:57 +0800

    Expose built-in map dimensions

--- src/map-package/constants.js
+++ src/map-package/constants.js
@@ -2,6 +2,8 @@ export const BUILT_IN_LANZHOU_MAP = Object.freeze({
   id: 'northern-song-lanzhou-1104',
   version: '1.0.5',
   title: '北宋兰州城 · RPG 战术地图',
+  width: 6000,
+  height: 5000,
 });
 
 export const DEFAULT_REFERENCE_MAP_ID = BUILT_IN_LANZHOU_MAP.id;
