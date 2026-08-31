b8008ebc7962fb2efdba99dcb417f166b5066ee1
王鑫磊 <64979279+Xlking152@users.noreply.github.com>
2026-08-31 20:51:59 +0800

    Keep registry metadata stable while exposing map bounds

--- src/map-package/constants.js
+++ src/map-package/constants.js
@@ -2,6 +2,9 @@ export const BUILT_IN_LANZHOU_MAP = Object.freeze({
   id: 'northern-song-lanzhou-1104',
   version: '1.0.5',
   title: '北宋兰州城 · RPG 战术地图',
+});
+
+export const BUILT_IN_LANZHOU_MAP_BOUNDS = Object.freeze({
   width: 6000,
   height: 5000,
 });
